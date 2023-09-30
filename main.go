0b40e7a0e977b3df8562a94707f3a3200036f429
Armando Salazar <armandosalazar351@gmail.com>
2023-09-29 22:10:56 -0600

    feat: add features

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"net/http"
+	"reflect"
+)
+
+func hi(name string) string {
+	temp := "temp"
+	// Una variable booleana no declarada es falso por defecto
+	// int8, int16, int32, int64
+	// uint8, ...
+	// float32, float64
+	if len(name) > 10 {
+		fmt.Println("long name" + temp)
+	}
+	var numbers [3]int
+	numbers[2] = 10
+	return "Hi " + name
+}
 
 func main() {
-	fmt.Println("Hi!")
+	fmt.Println(math.MaxInt64)
+
+	// TypeOf
+	fmt.Println(reflect.TypeOf(1))
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		_, err := writer.Write([]byte("Hi"))
+		if err != nil {
+			return
+		}
+	})
+
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		return
+	} else {
+		fmt.Println("Server running...")
+	}
 }
