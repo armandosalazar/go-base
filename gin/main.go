4d8848a1a5388dae08235e13542bc5ed3588629e
Armando Salazar <armandosalazar351@gmail.com>
2024-02-22 14:12:50 -0600

    build: add dependencies

--- gin/main.go
+++ gin/main.go
@@ -1,9 +1,60 @@
 package gin
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func Run() {
-	r := gin.Default()
+	router := gin.Default()
 
-	r.Run()
+
+	router.GET("/ping", func(c *gin.Context) {
+		fmt.Println("[*] request here")
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.GET("/ping/v2", ping)
+
+	router.GET("/cookie", func(c *gin.Context) {
+		cookie, err := c.Cookie("gin_cookie")
+
+		if err != nil {
+			// cookie = "NotSet"
+			c.SetCookie(
+				"gin_cookie",
+				"test",
+				3600,
+				"/",
+				"localhost",
+				false,
+				true,
+			)
+	
+		}
+
+	})
+
+	router.GET("/get-cookies", handlerGetCookies)
+
+	router.Run()
 }
+
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+func handlerGetCookies(c *gin.Context) {
+	cookie, err := c.Cookie("gin_cookie")
+
+	if err != nil {
+		fmt.Println("[!] error")
+	}
+
+	fmt.Println("[*] cookie value: ", cookie)
+}
+
